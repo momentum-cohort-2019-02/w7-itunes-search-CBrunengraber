f2ea522ba25a367aec914d4020da0d7bf3701723
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-29 13:53:28 -0400

    button hooked up, and able to console log different events in javascript

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    const input = document.querySelector('#search');
+    let type = document.getElementsByName('searchInput')
+    let button = document.getElementById("buttonSearch");
+    console.log(button, input, type);
+    myClick();
+});
+
 let jackJohnson = fetch("https://itunes-api-proxy.glitch.me/artist/jack-johnson/909253?uo=4")
 console.log('got the link')
+
+
+
+
+
+// Event Listener on the Button and it's logic
+function myClick(buttonSearch){
+document.getElementById("buttonSearch").addEventListener("click",function(event){
+    // alert("button clicked"); - great test - take out once works
+    event.stopPropagation();
+    });
+}
