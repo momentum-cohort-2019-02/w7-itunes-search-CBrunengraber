97d3e60b0d7dc41b060012f6ce26f420f61d7d84
Craig Brunengraber <brunengraber@hotmail.com>
2019-03-31 13:39:30 -0400

    trying to read json

--- main.js
+++ main.js
@@ -7,12 +7,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     myClick();
 });
 
-let jackJohnson = fetch("https://itunes-api-proxy.glitch.me/artist/jack-johnson/909253?uo=4")
-console.log('got the link')
-
-
-
-
+// let jackJohnson = fetch("https://itunes-api-proxy.glitch.me/artist/jack-johnson/909253?uo=4")
+// console.log('got the link')
 
 // Event Listener on the Button and it's logic
 function myClick(buttonSearch){
@@ -20,4 +16,20 @@ document.getElementById("buttonSearch").addEventListener("click",function(event)
     // alert("button clicked"); - great test - take out once works
     event.stopPropagation();
     });
+}
+
+// get text from input field to construct url - check chins hint
+
+//first get dang file
+//how to parse data from json file
+
+
+function dataParse(notSureWhatThisShouldBe){
+    return fetch("https://itunes-api-proxy.glitch.me/artist/jack-johnson/909253?uo=4")
+    .then(function(response) {
+        if (!response.ok) {
+            throw Error(response.statusText)
+        }
+        return response.json();
+    })
 }
